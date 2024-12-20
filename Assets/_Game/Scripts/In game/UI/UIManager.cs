adcafb03e58d59e3673eb770b08090c39bcbaf77
Aikokonut <145500500+Aikokonut@users.noreply.github.com>
2024-12-20 22:53:49 +0700

    vv

--- Assets/_Game/Scripts/In game/UI/UIManager.cs
+++ Assets/_Game/Scripts/In game/UI/UIManager.cs
@@ -1,27 +1,16 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
-    //[SerializeField] private Button googleSignInButton;
-
-    private void Start()
-    {
-        //googleSignInButton.onClick.AddListener(() =>
-        //{
-        //    // Gọi phương thức đăng nhập với Google
-        //    FirebaseManager.Instance.SignInWithGoogle("YOUR_ID_TOKEN");
-        //});
-    }
-
     public void Init()
     {
         Instance = this;
 
         this.UIMoveDisc.Init();
     }
-
     public UIPreThrow UIPreThrow;
     public UIAfterThrow UIAfterThrow;
     public UISessionComplete UISessionComplete;
